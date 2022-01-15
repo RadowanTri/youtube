ff518cf02cfe49d38f1d70394926fe0aa1788012
RadowanTri <radowan.bhuiyan.tri@gmail.com>
2022-01-15 22:43:24 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+var container1 = document.querySelector(".container1");
+
+menuIcon.onclick = function () {
+    sidebar.classList.toggle("small-sidebar");
+    container1.classList.toggle("large-container");
+
+}
+ 
+
